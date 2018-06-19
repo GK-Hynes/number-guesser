57f994ff403d08547bb04009cd008a6785fe92e7
Gerard Hynes <gkhynes@gmail.com>
2018-06-19 17:59:56 +0100

    Add lose condition and game over

--- app.js
+++ app.js
@@ -34,13 +34,27 @@ guessBtn.addEventListener("click", function() {
 
   // Check if won
   if (guess === winningNum) {
-    //Disable input
-    guessInput.disabled = true;
-    // Change border colour
-    guessInput.style.borderColor = "green";
-    // Set message
-    setMessage(`${winningNum} is correct! YOU WIN!`, "green");
+    // Game over - won
+    gameOver(true, `${winningNum} is correct! YOU WIN!`);
   } else {
+    // Wrong number
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      // Game over - lost
+      gameOver(
+        false,
+        `Game Over. You lost. The correct number was ${winningNum}`
+      );
+    } else {
+      // Game continues - answer wrong
+      guessInput.style.borderColor = "red";
+      // Clear the input
+      guessInput.value = "";
+
+      // Alert user
+      setMessage(`${guess} is not correct. ${guessesLeft} guesses left`, "red");
+    }
   }
 });
 
@@ -48,4 +62,19 @@ guessBtn.addEventListener("click", function() {
 function setMessage(msg, color) {
   message.style.color = color;
   message.textContent = msg;
+}
+
+// Game over
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+
+  //Disable input
+  guessInput.disabled = true;
+  // Change border colour
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
 }
